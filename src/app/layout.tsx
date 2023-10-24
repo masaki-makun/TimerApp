96b4018c62a3642f2381e053fee41c1522329495
masaki-makun <monsterexile@yahoo.co.jp>
2023-10-24 19:53:27 +0900

    change page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Favicon from "/public/images/favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Timer App",
   description: "Timer application using Next.js.",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
